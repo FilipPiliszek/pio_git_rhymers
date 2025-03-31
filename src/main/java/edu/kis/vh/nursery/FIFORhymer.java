81c459d1b91c4bb049f95f002db2e4705af0687c
Filip Piliszek <252819@edu.p.lodz.pl>
2025-03-31 12:59:00 +0200

    Dodanie dokumentacji do klasy FIFORhymer

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,8 +1,18 @@
 package edu.kis.vh.nursery;
+/*
+    Klasa reprezentuje specjalizowaną implementację DefaultCountingOutRhymer,
+    która stosuje algorytm FIFO (First-In-First-Out) do zwracania elementów.
+ */
 
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
+
+    //Tymczasowy schowek używany do odwracania kolejności elementów.
+
     public final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
+
+
+    //Nadpisuje metodę countOut, aby zaimplementować zachowanie FIFO.
 
     @Override
     public int countOut() {
